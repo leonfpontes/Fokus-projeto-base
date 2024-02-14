3f59aba9385165ebc97375f052fce576656a00fa
leonardo.pontes <leonardo.pontes@mag-it.com.br>
2024-02-14 15:28:35 -0300

    concluir tarefa com custom event

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ musicaFocoInput.addEventListener('change', () => {
 })
 
 focoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 1500
+    tempoDecorridoEmSegundos = 5
     alterarContexto('foco')
     focoBt.classList.add('active')
 })
@@ -79,6 +79,11 @@ const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0){
         audioTempoFinalizado.play()
         alert('Tempo finalizado!')
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco'
+        if (focoAtivo){
+            const evento = new CustomEvent('FocoFinalizado')
+            document.dispatchEvent(evento)
+        }
         zerar()
         return
     }
