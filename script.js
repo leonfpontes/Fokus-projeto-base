995e5e373dddca6461e5d048b378aa85f4a0fede
leonardo.pontes <leonardo.pontes@mag-it.com.br>
2024-02-14 10:12:05 -0300

    Temporizador por contexto.

--- script.js
+++ script.js
@@ -10,12 +10,13 @@ const musicaFocoInput = document.querySelector('#alternar-musica')
 const iniciarOuPausarBt =  document.querySelector('#start-pause span')
 const iniciarOuPausarImg = document.querySelector('.app__card-primary-butto-icon')
 const tempoNaTela = document.querySelector('#timer')
+
 const musica = new Audio('./sons/luna-rise-part-one.mp3')
 const iniciarContador = new Audio('./sons/play.wav')
 const pausarContador = new Audio('./sons/pause.mp3')
 const fimDoContador = new Audio('./sons/beep.mp3')
 
-let tempoDecorridoEmSegundos = 5
+let tempoDecorridoEmSegundos = 1500
 let intervaloId = null
 
 musica.loop = true
@@ -29,21 +30,26 @@ musicaFocoInput.addEventListener('change', () => {
 })
 
 focoBt.addEventListener('click', () =>{
+    tempoDecorridoEmSegundos = 1500
     alterarContexto('foco')
     focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () =>{
+    tempoDecorridoEmSegundos = 300
     alterarContexto('descanso-curto')
     curtoBt.classList.add('active')
+    
 })
 
 longoBt.addEventListener('click', () =>{
+    tempoDecorridoEmSegundos = 900
     alterarContexto('descanso-longo')
     longoBt.classList.add('active')
 })
 
 function alterarContexto (contexto) {
+    mostrarTempo()
     botoes.forEach(function(contexto){
         contexto.classList.remove('active')
     })
@@ -81,7 +87,7 @@ const contagemRegressiva = () => {
         return
     }
     tempoDecorridoEmSegundos -= 1
-    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
+    mostrarTempo()
 }
 
 startPauseBt.addEventListener('click', iniciarOuPausar)
@@ -104,3 +110,11 @@ function zerar() {
     iniciarOuPausarImg.src ="/imagens/play_arrow.png"
     intervaloId = null
 }
+
+function mostrarTempo () {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo()
